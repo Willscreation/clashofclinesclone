1b6f6a346319e24526d5d9bb64736321d5c1c981
Vineethmonis <vineeth.monis098@gmail.com>
2024-10-30 09:24:54 +0530

    new

--- script.js
+++ script.js
@@ -40,4 +40,14 @@ function logout() {
     document.getElementById('logout-button').classList.add('hidden');
 }
 
+document.getElementById("show-register").addEventListener("click", function() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("register-form").style.display = "block";
+});
+
+document.getElementById("show-login").addEventListener("click", function() {
+    document.getElementById("register-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+});
+
 
